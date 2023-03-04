5310d6a58bff5cf7ebef1db9d44661c75ea06bca
muhammad saim <muhammad.saim@discorp.com>
2023-03-04 23:27:59 +0500

    added solution for valid palindrome two problem

--- src/main/java/com/pattern/two/pointers/ValidPalindromeTwo.java
+++ src/main/java/com/pattern/two/pointers/ValidPalindromeTwo.java
@@ -1,12 +1,50 @@
 package com.pattern.two.pointers;
 
 /**
- * Note: Write a function that takes a string as input and checks whether it can be a valid palindrome by removing at most one character from it
+ * Note: Write a function that takes a string as input and checks whether it can be a
+ * valid palindrome by removing at most one character from it
  */
 public class ValidPalindromeTwo {
 
-    public static void main(String[] args) {
+    public static boolean isPalindrome(String s) {
+        int start = 0;
+        int end = s.length() - 1;
+        while (start < end) {
+            if (s.charAt(start) != s.charAt(end)) {
+                //check by removing from the end
+                return isValidPalindrome(s, start, end - 1)
+                        || isValidPalindrome(s, start + 1, end);
+            }
+            start = start + 1;
+            end = end - 1;
+        }
+        return true;
+    }
 
+    public static boolean isValidPalindrome(String s, int start, int end) {
+        while (start < end) {
+            if (s.charAt(start) != s.charAt(end)) {
+                return false;
+            }
+            start = start + 1;
+            end = end - 1;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String[] testCases = {
+                "madam",
+                "madame",
+                "dead",
+                "abca",
+                "tebbem",
+                "eeccccbebaeeabebccceea",
+                "ognfjhgbjhzkqhzadmgqbwqsktzqwjexqvzjsopolnmvnymbbzoofzbbmynvmnloposjzvqxejwqztksqwbqgmdazhqkzhjbghjfno"
+        };
+        for (String testCase : testCases) {
+            System.out.println(ValidPalindromeTwo.isPalindrome(testCase));
+        }
     }
 }
 
